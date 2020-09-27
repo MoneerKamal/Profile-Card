c65524b5e19a629ef33314c73ccdeeb328f6f787
Moneer Kamal <moneer.kamal@tatweer.sy>
2020-09-27 15:07:19 +0300

    init project

---
+++ public_html/scripts.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var messageBox = document.querySelector('.js-message');
+  var btn = document.querySelector('.js-message-btn');
+  var card = document.querySelector('.js-profile-card');
+  var closeBtn = document.querySelectorAll('.js-message-close');
+
+  btn.addEventListener('click',function (e) {
+      e.preventDefault();
+      card.classList.add('active');
+  });
+
+  closeBtn.forEach(function (element, index) {
+     console.log(element);
+      element.addEventListener('click',function (e) {
+          e.preventDefault();
+          card.classList.remove('active');
+      });
+  });
